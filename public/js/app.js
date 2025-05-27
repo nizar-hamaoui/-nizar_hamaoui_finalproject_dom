08383c44165edeab80f2b5332152a9286dc0eda9
ame <glomark779@gmail.com>
2025-05-27 16:50:09 +0100

    edit section number3

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,17 @@
 let iconVD = document.querySelector('#icon-vd')
 let dvVD = document.querySelector('.vd-watch')
 let wtCL = document.querySelector('#watch-close')
+let db = document.querySelector('body')
 iconVD.addEventListener('click', ()=>{
     dvVD.style.display = 'flex'
+    db.style.overflow = 'hidden'
+    
 
 })
 
 wtCL.addEventListener('click', ()=>{
     dvVD.style.display = 'none'
+    db.style.overflow = 'scroll'
 })
 
 // code dyal login
@@ -19,7 +23,9 @@ let logCl = document.querySelector('#log-close')
 
 logg.addEventListener('click', ()=>{
     divLog.style.display = 'flex'
+    db.style.overflow = 'hidden'
 })
 logCl.addEventListener('click', ()=>{
     divLog.style.display = 'none'
+    db.style.overflow = 'scroll'
 })
